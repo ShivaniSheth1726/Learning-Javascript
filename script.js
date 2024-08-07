1811aa683e07337b7e45801017c431c849db6acc
ShivaniSheth1726 <165990555+ShivaniSheth1726@users.noreply.github.com>
2024-08-07 06:50:12 +0000

    improve code for conditional statement

--- script.js
+++ script.js
@@ -42,14 +42,35 @@ if(fruit.includes("a")||fruit.includes("A"))
     
 // }
 
-const name=prompt("Enter your Name");
 
-if(name.includes(" ")&&(name.at(-1)!==" "))
-{
-    console.log('its a full name');
+
+// if(name.includes(" ")&&(name.at(-1)!==" "))
+// {
+//     console.log('its a full name');
+// }
+// else{
+//     console.log('Enter your Full Name');   
+// }
+
+
+const fullName=prompt("Enter your Name");
+// if(fullName.includes(" ")){
+//     if(fullName.at(-1)!==" "){
+//         console.log('This is the full Name');
+//     }else{
+//         console.log('enter your full name');
+//     }
+// }else{
+//     console.log('enter your full Name');
+    
+// }
+
+
+
+const userInput=fullName.trim();
+if(userInput.includes(" ")){
+    console.log('Its a full name');
+}else{
+    console.log('Enter your full Name');
+    
 }
-else{
-    console.log('Enter your Full Name');   
-}
-
-
